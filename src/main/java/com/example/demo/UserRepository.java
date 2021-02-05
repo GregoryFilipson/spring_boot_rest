48b352557c177cf6dbaeac46792837ce958cfae1
GregoryFilipson <alivenemo@gmail.com>
2021-02-05 08:53:20 +0300

    added new text in InvalidCredentials.java and UnauthorizedUser.java

--- src/main/java/com/example/demo/UserRepository.java
+++ src/main/java/com/example/demo/UserRepository.java
@@ -11,19 +11,13 @@ import java.util.List;
 public class UserRepository {
     HashMap<User, List<Authorities>> map = new HashMap<>();
 
-//    public UserRepository() {
-//        User user1 = new User("Петя", "123");
-//        map.put(user1, Arrays.asList(Authorities.READ, Authorities.WRITE));
-//    }
+    public UserRepository() {
+        User user1 = new User("Петя", "123");
+        map.put(user1, Arrays.asList(Authorities.READ, Authorities.WRITE));
+    }
 
     public List<Authorities> getUserAuthorities(String userName, String password) {
         User user = new User(userName, password);
         return map.get(user);
-    }
-
-    @PostConstruct
-    public void userInit() {
-        User user1 = new User("Петя", "123");
-        map.put(user1, Arrays.asList(Authorities.READ, Authorities.WRITE));
     }
 }
